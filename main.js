6dcdbda4d3fed06aed6e2b07f2a4ea09a8edc1ec
sosoosVibez <sophie.bartelt@gmail.com>
2025-12-21 21:13:08 +0100

    grid created

---
+++ main.js
@@ -0,0 +1,36 @@
+setup();
+
+function setup() {
+  let container = document.querySelector(".container");
+  for (let index = 0; index < 16; index++) {
+    let row = createDivRow(container);
+    container.appendChild(row);
+  }
+  //makeFlexStyle(container);
+}
+
+function createDivRow(parent) {
+  console.log("createDivRow");
+  let row = document.createElement("div");
+  makeFlexStyle(row);
+  for (let index = 0; index < 16; index++) {
+    let div = createDiv(row);
+    row.appendChild(div);
+  }
+  return row;
+}
+
+function createDiv(parent) {
+  let div = document.createElement("div");
+  div.style.flex = "1";
+  return div;
+}
+
+function makeFlexStyle(parent) {
+  parent.style.display = "flex";
+  parent.style.flexDirection = "row";
+  parent.style.alignItems = "stretch";
+  //parent.style.justifyContent = "flex-end";
+  parent.style.height = "1rem";
+  parent.style.width = "100%";
+}
